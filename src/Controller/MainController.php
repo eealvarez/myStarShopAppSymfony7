ba1c72f5c59fbf7ed3aed63b16ed0ab5af6a0a51
eealvarez <familia_alvarez>
2024-10-19 20:27:15 -0600

    Agregada mi primera plantilla y enviada variables a la misma

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-        return new Response('<strong>INFUNISA Starshop</strong>: ¡Tu tienda de accesorios de cómputo!');
+        // return new Response('<strong>INFUNISA Starshop</strong>: ¡Tu tienda de accesorios de cómputo!');
+
+        $numeroDeComputadoras = 457;
+
+        $myPC = [
+            'marca' => 'Dell',
+            'CPU' => '3.2 GHz',
+            'RAM' => '16 GB',
+            'HDD' => '1 TB',
+        ];
+
+        return $this->render('main/index.html.twig', [
+            'numeroDeComputadoras' => $numeroDeComputadoras,
+            'myPC' => $myPC,
+        ]);
     }
 }
